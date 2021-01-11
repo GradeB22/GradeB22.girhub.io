37b3e3584cffe6ad1a3a5804d547360ffdf5b228
GradeB22 <77265452+GradeB22@users.noreply.github.com>
2021-01-11 14:38:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var header = `
+		<header>
+			<label style="font-size:25px; color:white;"><b>(тут буде логотип і посилання)</b></label>
+			<nav style="padding-top:6px;">
+				<a href="home.html" style="padding-right:10px;">Головна</a>
+				<a href="oops.html" style="padding-right:10px;">Блоги</a>
+				<a href="oops.html" style="padding-right:10px;">Чат</a>
+				<a href="news.html" style="padding-right:10px;">Новини</a>
+				<a href="oops.html" style="padding-right:10px;">Домашнє завдання</a>
+				<a href="#">Інше</a>
+			</nav>
+		</header><br><br>
+`
+
+document.write(header)
